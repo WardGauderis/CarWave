04e01c64e7a53c0aaff98192ba2be10376cdde74
Ward <ward.gauderis@student.uantwerpen.be>
2020-02-21 18:22:41 +0100

    sql fix

--- config.py
+++ config.py
@@ -6,4 +6,6 @@ class Config:
     Config class with values that is passed to the flask application
     """
 
-    SQLALCHEMY_TRACK_URI = os.environ.get('DATABASE_URI') or 'psql:/// whatever postgres uri is'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'psql:/// whatever postgres uri is'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
