077fc7e1b092bb0af46833412f005b095e4396c7
Hayaan Abdi <hayaanabdi@outlook.com>
2020-02-23 19:20:39 +0100

    squash soon

--- app/models.py
+++ app/models.py
@@ -5,13 +5,16 @@ from datetime import datetime
 File with the database models described using SQLAlchemy
 """
 
+# TODO: address & rides relationships
+
 # Relationships
 
+
 belongs_to = db.Table(
-    db.Column(
-        "driver_id", db.Integer, db.ForeignKey("drivers.id"), primary_key=True
-    ),
-    db.Column("car_id", db.Integer, db.ForeignKey("cars.id"), primary_key=True),
+    "belongs_to",
+    db.metadata,
+    db.Column("driver_id", db.Integer, db.ForeignKey("drivers.id")),
+    db.Column("car_id", db.Integer, db.ForeignKey("cars.id")),
 )
 
 
@@ -19,21 +22,18 @@ belongs_to = db.Table(
 
 
 class User(db.Model):
-    """
-    """
-
     __tablename__ = "users"
+
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String, nullable=False)
     last_name = db.Column(db.String, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    # TODO: FK
-    address_id = db.Column(db.Integer, db.ForeignKey("addresses.id"))
+    address_id = db.Column(db.Integer, db.ForeignKey("addresses.id"))  # m2m
     phone_number = db.Column(db.String)
 
-    @staticmethod
-    def get_user(id: int):
-        return None
+    def __repr__(self):
+        # return f"<User(id={self.id}, name={self.first_name + ' ' + self.last_name})>"
+        pass
 
 
 class Driver(User):
@@ -42,7 +42,13 @@ class Driver(User):
     """
 
     __tablename__ = "drivers"
+
+    id = db.Column(db.Integer, db.ForeignKey("users.id"), primary_key=True)
     rating = db.column(db.Integer, nullable=False)
+    cars = db.relationship("Car", secondary=belongs_to, back_populates="cars")
+
+    def __repr__(self):
+        pass
 
 
 class Passenger(User):
@@ -51,44 +57,55 @@ class Passenger(User):
     """
 
     __tablename__ = "passengers"
+
+    id = db.Column(db.Integer, db.ForeignKey("users.id"), primary_key=True)
     rating = db.column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        pass
 
 
 class Ride(db.Model):
-    """
-    """
-
     __tablename__ = "rides"
+
     id = db.Column(db.Integer, primary_key=True)
-    # driver_id = db.relationship() one2m
-    # passenger_id = db.relationship() m2m
+    driver_id = db.Column(  # one2m
+        db.Integer, db.ForeignKey("drivers.id"), nullable=False
+    )
+    passenger_id = db.Column(  # m2m
+        db.Integer, db.ForeignKey("passengers.id"), nullable=False
+    )
     request_time = db.Column(
         db.DateTime, default=datetime.utcnow, nullable=False
     )
     departure_time = db.Column(db.DateTime, nullable=False)
     arrival_time = db.Column(db.DateTime)
 
+    def __repr__(self):
+        pass
+
 
 class Address(db.Model):
-    """
-    """
-
     __tablename__ = "addresses"
+
     id = db.Column(db.Integer, primary_key=True)
+    # FIXME: unique. index?
+    address = db.Column(db.String, unique=True, nullable=False)
+
+    def __repr__(self):
+        pass
 
 
 class Car(db.Model):
-    """
-    """
-
     __tablename__ = "cars"
+
     license_plate = db.column(db.String, primary_key=True)
     model = db.column(db.String, nullable=True)
     colour = db.column(db.String, nullable=True)
     num_passengers = db.column(db.Integer, nullable=False)
-    belongs_to = db.relationship(
-        "?", # TODO:
-        secondary=belongs_to,
-        lazy="subquery",
-        backref=db.backref("pages", lazy=True),
+    owners = db.relationship(
+        "Driver", secondary=belongs_to, back_populates="drivers"
     )
+
+    def __repr__(self):
+        pass
--- config.py
+++ config.py
@@ -6,6 +6,5 @@ class Config:
     Config class with values that is passed to the flask application
     """
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URI') or 'postgresql://app@localhost/CarWave or whatever postgres uri is'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
