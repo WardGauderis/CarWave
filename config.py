53d4e888155ce662289b92f0adef10fd184d54df
sharogue <mano.marichal@student.uantwerpen.be>
2020-02-28 13:41:18 +0100

    bootystrap

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ class Config:
     """
 
     SQLALCHEMY_DATABASE_URI = environ.get(
-        'DATABASE_URI') or 'postgresql://app@localhost/carwave_db?user=postgres&password=password'
+        'DATABASE_URI') or 'postgresql://app@localhost/carwave_db?user=postgres&password=hallo'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = environ.get('SECRET_KEY') or b'\xdd/\xb0f\x0co\xe06O\xf6.\xac\xbe)\xcd\xb7'
