5db3a29e64ab80857696b08e42582c004f669c8f
Hayaan Abdi <hayaanabdi96+github@gmail.com>
2020-03-04 09:46:50 +0100

    format -> f string

--- app/errors/errors.py
+++ app/errors/errors.py
@@ -1,17 +1,21 @@
 from flask import jsonify, request
-from werkzeug.http import HTTP_STATUS_CODES
 from werkzeug.exceptions import HTTPException
+from werkzeug.http import HTTP_STATUS_CODES
+
 from app.errors import bp
 
 
 def json_error():
-    return request.accept_mimetypes['application/json'] >= request.accept_mimetypes['text/html']
+    return (
+        request.accept_mimetypes["application/json"]
+        >= request.accept_mimetypes["text/html"]
+    )
 
 
 def api_error(status_code, description=None):
-    payload = {'error': HTTP_STATUS_CODES.get(status_code, 'Unknown error')}
+    payload = {"error": HTTP_STATUS_CODES.get(status_code, "Unknown error")}
     if description:
-        payload['description'] = description
+        payload["description"] = description
     response = jsonify(payload)
     response.status_code = status_code
     return response
@@ -22,5 +26,7 @@ def not_found(error):
     if json_error():
         return api_error(error.code, error.description)
     else:
-        return "DiT iS eEn MoOie ErOr PaGiNa VoOr ErRoR {}: {}<br>{}".format(
-            error.code, HTTP_STATUS_CODES.get(error.code, 'Unknown error'), error.description), error.code
+        return (
+            f"DiT iS eEn MoOie ErOr PaGiNa VoOr ErRoR {error.code}: {HTTP_STATUS_CODES.get(error.code, 'Unknown error')}<br>{error.description}",
+            error.code,
+        )
