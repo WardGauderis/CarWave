0dabef6e6f6bc25038ef7405d6273d9b15012f0e
funkymaniac <thomas@dooms.eu>
2020-03-09 15:09:46 +0100

    login&such

--- app/map/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
-
-
-class WaypointsForm(FlaskForm):
-    start = StringField('start', [DataRequired()])
-    end = StringField('end', [DataRequired()])
--- app/map/routes.py
+++ app/map/routes.py
@@ -1,8 +1,6 @@
 from flask import render_template
 from app.map import bp
-from app.map.forms import WaypointsForm
 
 @bp.route('/map')
 def map():
-    form = WaypointsForm()
-    return render_template('map.html', title='Map', form=form)
+    return render_template('map.html', title='Map')
