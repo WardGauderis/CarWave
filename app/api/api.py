cbfd4726d08544ec11983696735b03c19ac37aee
Ward <ward.gauderis@student.uantwerpen.be>
2020-04-17 12:57:44 +0200

    fix form

--- app/api/api.py
+++ app/api/api.py
@@ -171,6 +171,7 @@ def accept_passenger_request(drive_id, user_id):
     else:
         abort(401, "Invalid authorization")
 
+
 # TODO search drive -> doe dit in de crud functie search_drives() zodate de site dit ook kan gebruiken
 @bp.route("/drives/search", methods=["GET"])
 def search_drive():
--- app/offer/forms.py
+++ app/offer/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField
-from wtforms.fields import IntegerField, FloatField, DateTimeField
+from wtforms.fields import IntegerField, FloatField, StringField
 from wtforms.validators import DataRequired, NumberRange, ValidationError
 from app.forms import DictForm
 from datetime import datetime
@@ -13,15 +13,12 @@ class OfferForm(DictForm):
     to_lon = FloatField('', [DataRequired(), NumberRange(-180, 180)])
     to_lat = FloatField('', [DataRequired(), NumberRange(-90, 90)])
 
-    arrival_time = DateTimeField('arrival time', [DataRequired()], "%Y-%m-%dT%H:%M:%S%z")
+    arrival_time = StringField('arrival time', [DataRequired()])
     passenger_places = IntegerField('number of passengers', [DataRequired(), NumberRange(1)])
     confirm = SubmitField('confirm')
 
     def from_json(self, json):
-        try:
-            self.arrival_time.data = dateutil.parser.isoparse(json.get('arrive-by'))
-        except:
-            self.all_errors['arrive-by'] = "Not a valid date format"
+        self.arrival_time.data = json.get('arrive-by')
         self.passenger_places.process_formdata([json.get('passenger-places', 0)])
         try:
             self.from_lat.data = float(json.get('from')[0])
@@ -36,6 +33,12 @@ class OfferForm(DictForm):
         return self.validate_json()
 
     def validate_arrival_time(self, arrival_time):
+        try:
+            arrival_time.data = dateutil.parser.isoparse(arrival_time.data)
+        except:
+            self.all_errors['arrive-by'] = "Not a valid date format"
+            return
+
         if arrival_time.data <= datetime.utcnow():
             raise ValidationError('Arrival time must be in the future')
 
