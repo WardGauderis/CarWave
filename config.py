51188fec6aff6347e8ae4a01ceb2ef3ea61fafa7
sharogue <mano.marichal@student.uantwerpen.be>
2020-04-20 15:57:39 +0200

    about uwu

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
     MAIL_USERNAME = 'hallobesteploppers@gmail.com'
     MAIL_DEFAULT_SENDER = 'hallobesteploppers@gmail.com'
     MAIL_PASSWORD = 'j3icT8XWy9FB7ig'
-
+    TEMPLATES_AUTO_RELOAD = True
     SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URI") or 'postgresql://app@localhost/carwave_db?user=postgres&password=postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = (environ.get("SECRET_KEY") or b"\xdd/\xb0f\x0co\xe06O\xf6.\xac\xbe)\xcd\xb7")
